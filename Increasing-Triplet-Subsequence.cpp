360889e9ca53dc3a751e5e6d7b823185e29eb4fd
M.Naranbat <munkherdene.naranbat@gmail.com>
2023-07-03 02:19:50 +0800

    feat: good problem

---
+++ Increasing-Triplet-Subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    bool increasingTriplet(vector<int> &nums)
+    {
+
+        int right = 0, left = INT_MAX, mid = INT_MAX;
+
+        for (right; right < nums.size(); right++)
+        {
+            if (nums[right] > mid)
+                return true;
+            else if (nums[right] < mid && nums[right] > left)
+            {
+                mid = nums[right];
+            }
+            else if (nums[right] < left)
+            {
+                left = nums[right];
+            }
+        }
+
+        return false;
+    }
+};
