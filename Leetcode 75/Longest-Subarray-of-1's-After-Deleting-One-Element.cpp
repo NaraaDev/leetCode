aa5a5ce0805f522702a75357046e930ba566921e
Naranbat <massive0304@gmail.com>
2023-07-11 23:01:35 +0800

    feat: leetcode 75 sliding window last problem

---
+++ Leetcode 75/Longest-Subarray-of-1's-After-Deleting-One-Element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        
+        int size = nums.size(), ans = 0, cnt = 0, preZeroPosition = -1;
+
+        for(int i = 0; i < size; i++) {
+
+            if(nums[i] == 0) {
+                if(preZeroPosition == -1) 
+                    preZeroPosition = i;
+                else {
+                    cnt = i - preZeroPosition - 1;
+                    preZeroPosition = i;
+                }
+            } 
+            else 
+                cnt++;
+            ans = max(ans, cnt);
+        }
+        return (ans == size ? size - 1 : ans);
+    }
+};
